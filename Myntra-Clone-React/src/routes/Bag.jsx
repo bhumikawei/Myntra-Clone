d36ab118c8c0d0dce28a3be97ea134f54b262522
Bhumika <bhumika04shimla@gmail.com>
2025-10-08 14:38:03 +0530

    made bag ui and also done quite corrections ig

--- Myntra-Clone-React/src/routes/Bag.jsx
+++ Myntra-Clone-React/src/routes/Bag.jsx
@@ -1,14 +1,33 @@
+import BagItemContainer from "../components/Bag-Item-Container"
+import BagSummary from "../components/BagSummary"
 
 
 const Bag = () => {
+  const item = {  
+        id: '001',
+        image: 'images/1.jpg',
+        company: 'Carlton London',
+        item_name: 'Rhodium-Plated CZ Floral Studs',
+        original_price: 1045,
+        current_price: 606,
+        discount_percentage: 42,
+        return_period: 14,
+        delivery_date: '10 Oct 2023',
+        rating: {
+            stars: 4.5,
+            count: 1400,
+        
+    }
+    }; 
   return (
     <>
      <main>
       <div className="bag-page">
         <div className="bag-items-container">
+          <BagItemContainer item={item}/>
         </div>
-        <div className="bag-summary">
-        </div>
+        
+         <BagSummary/>
 
       </div>
      </main>
