438b006fcfd53c0bb0a0f418ac55693bdf14dc2a
Bhumika <bhumika04shimla@gmail.com>
2025-10-08 20:41:48 +0530

    backend server for spinning loader

--- Myntra-Clone-React/src/routes/App.jsx
+++ Myntra-Clone-React/src/routes/App.jsx
@@ -4,15 +4,20 @@ import {Outlet} from 'react-router-dom'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import FetchItems from '../components/FetchItems'
+import { useSelector } from 'react-redux'
+import LoadingSpinner from '../components/LoadingSpinner'
 
 
 function App() {
+
+  const fetchStatus = useSelector((store) => store.fetchStatus);
 
   return (
     <>
      <Header />
      <FetchItems/>
-     <Outlet />
+     {fetchStatus.currentlyFetching ? <LoadingSpinner/> : <Outlet />}
+     
      <Footer/>
     </>
   )
